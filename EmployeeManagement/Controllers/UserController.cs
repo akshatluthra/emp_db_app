8913620fc1d2002b8ce4305eaffd0ba21a06a40d
ug123458 <ug123458@gmail.com>
2023-06-12 16:38:42 +0530

    Completed task

---
+++ EmployeeManagement/Controllers/UserController.cs
@@ -0,0 +1,162 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using EmployeeManagement.Repositories;
+
+namespace EmployeeManagement.Controllers
+{
+    public class UserController : Controller
+    {
+            private readonly IConfiguration _configuration;
+        private readonly UserRepository _userRepository;
+        public UserController(IConfiguration configuration,UserRepository userRepository)
+        {
+            _configuration = configuration;
+            _userRepository = userRepository;
+        }
+        [HttpPost]
+        [Route("login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            if (login == null)
+            {
+                return BadRequest();
+            }
+
+
+            var user = this._userRepository.GetUserByEmail(login.Email);
+
+            if (user == null)
+            {
+
+                return NotFound();
+            }
+
+            if(user.Password != login.Password)
+            {
+                return BadRequest("Invalid password");
+            }
+
+            var token = CreateToken(user);
+
+            return Ok(new { token = token });
+
+
+        }
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        [Authorize()]
+        public ActionResult<List<User>> GetAllUsers()
+        {
+            try
+            {
+                List<User> UsersList = this._userRepository.GetAllUsers();
+                return Ok(UsersList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("add-user")]
+        [ Authorize(Roles = "Admin")]
+        public ActionResult AddUser([FromBody] User user)
+        {
+            try
+            {
+                Console.WriteLine(user.Email);
+                this._userRepository.RegisterUser(user);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {   
+                Console.WriteLine(ex.ToString());
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+        [HttpGet]
+        [Route("get-user/{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            try
+            {
+                User userindb = this._userRepository.GetUserById(id);
+
+                if(userindb == null) {
+                    return BadRequest("user not found");
+                }
+                return Ok(userindb);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("update-user"), Authorize(Roles = "Admin")]
+        public ActionResult UpdateUser([FromBody] User emp)
+        {
+            try
+            {
+                 this._userRepository.UpdateUser(emp);
+               
+                return Ok("User updated");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete-user/{id}"), Authorize(Roles = "Admin")]
+        public ActionResult DeleteUser(int id)
+        {
+            try
+            {
+                this._userRepository.DeleteUser(id);
+               
+                return Ok("User Deleted Successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+    }
+
+}
---
+++ EmployeeManagement/Repositories/UserRepository.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using EmployeeManagement.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace EmployeeManagement.Repositories
+{
+
+    public class UserRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private string GetConnectionString()
+        {
+            return configuration.GetConnectionString("MyDbConnection");
+        }
+
+        private SqlConnection GetConnection()
+        {
+            string connectionString = GetConnectionString();
+            return new SqlConnection(connectionString);
+        }
+
+        public User GetUserById(int id)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+               
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        return GetUserFromSql(reader);
+                    }
+
+                    return null;
+                }
+                
+            }
+        
+    
+
+    public User GetUserByEmail(string email)
+    {
+            using (SqlConnection connection = GetConnection())
+            {
+               
+
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE Email = @Email", connection);
+                command.Parameters.AddWithValue("@Email", email);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return GetUserFromSql(reader);
+                }
+
+                return null;
+               
+            }
+    }
+
+    public void RegisterUser(User user)
+    {
+            using (SqlConnection connection = GetConnection())
+            {
+               
+
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Users (Name, Email, Role, Password) VALUES (@Name, @Email, @Role, @Password)", connection);
+                command.Parameters.AddWithValue("@Name", user.Name);
+                command.Parameters.AddWithValue("@Email", user.Email);
+                command.Parameters.AddWithValue("@Role", user.Role);
+                command.Parameters.AddWithValue("@Password", user.Password);
+                command.ExecuteNonQuery();
+               
+                    
+            }
+    }
+
+    public List<User> GetAllUsers()
+    {
+        List<User> Users = new List<User>();
+
+        using (SqlConnection connection = GetConnection())
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Users", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                User user = GetUserFromSql(reader);
+                Users.Add(user);
+            }
+        }
+
+        return Users;
+    }
+
+    public void UpdateUser(User user)
+    {
+        using (SqlConnection connection =GetConnection())
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("UPDATE Users SET Name = @Name, Email = @Email, Role = @Role, Password = @Password WHERE Id = @Id", connection);
+            command.Parameters.AddWithValue("@Id", user.Id);
+            command.Parameters.AddWithValue("@Name", user.Name);
+            command.Parameters.AddWithValue("@Email", user.Email);
+            command.Parameters.AddWithValue("@Role", user.Role);
+            command.Parameters.AddWithValue("@Password", user.Password);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public void DeleteUser(int id)
+    {
+        using (SqlConnection connection = GetConnection())
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("DELETE FROM Users WHERE Id = @Id", connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    private User GetUserFromSql(SqlDataReader reader)
+    {
+        return new User
+        {
+            Id = (int)reader["Id"],
+            Name = (string)reader["Name"],
+            Email = (string)reader["Email"],
+            Role = (string)reader["Role"],
+            Password = (string)reader["Password"]
+        };
+    }
+}
+    }
+
